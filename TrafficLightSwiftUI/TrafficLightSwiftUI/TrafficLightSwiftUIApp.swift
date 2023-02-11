4ae284b8b1a911d73010c30239b95c492128dda0
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2023-02-11 21:24:23 +0300

    11.02.2023

---
+++ TrafficLightSwiftUI/TrafficLightSwiftUI/TrafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightSwiftUIApp.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Евгений Забродский on 11.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
